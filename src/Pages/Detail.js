5a8af0903886f29865c03c4c676dc0f64e0d2f71
sumin34 <s201801744@hufs.ac.kr>
2023-09-05 19:04:26 +0900

    detail add

---
+++ src/Pages/Detail.js
@@ -0,0 +1,12 @@
+import '../App.css';
+import TotalCard from '../components/TotalCard';
+
+function Detail() {
+  return (
+    <>
+    <TotalCard></TotalCard>
+    </>
+  );
+}
+
+export default Detail;
