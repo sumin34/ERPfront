43b992264ae6adf8127b688f02f8fffdd492c88f
sumin34 <s201801744@hufs.ac.kr>
2023-10-20 17:02:18 +0900

    delete card2,3

--- src/components/CardBody.js
+++ src/components/CardBody.js
@@ -1,7 +1,5 @@
 import React from "react";
 import Card1 from "./Card1";
-import Card2 from "./Card2";
-import Card3 from "./Card3";
 import Card4 from "./Card4";
 
 function CardBody({className}) {
